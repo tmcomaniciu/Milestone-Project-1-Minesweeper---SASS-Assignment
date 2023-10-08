dbf059eeae1aacaa61363e9bf8e52c612c67703b
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-10-08 19:51:19 -0400

    JS reset function for level and timer

--- script.js
+++ script.js
@@ -73,4 +73,81 @@ const preGame = true;
 
 //code to create levels based on diffulty level pulled from board size in const = levels
 
-function createLevel(gameLevel) {}
+//code to create mines and free cells, randomize, and put together
+function createLevel(gameLevel) {
+  levelParameters = gameLevels[gameLevel];
+  const levelParts = levelParameters.split("x");
+  rows = parseInt(levelParts[0]);
+  columns = parseInt(levelParts[1]);
+  cellCount = rows * columns;
+  mines = parseInt(levelParts[2]);
+  emptyCells = cellCount - mines;
+}
+
+//function to create board, set up/build board using math above, pre-game
+function createBoard(gameLevel) {
+  //clear the board from previus game/ ensure ready for new player
+  gameGrid.innerHTML = statusIndicator;
+  gameGrid.classList.remove("disabled", "lose", "win");
+  gameGrid.classList.add("preGame");
+
+  //set up the grid from function above
+  createLevel(gameLevels);
+
+  //set the game for begin
+  preGame = true;
+
+  //build the level rows
+  for (const r = 0; r < rows; r++) {
+    const newTiles = "";
+    //build the tiles
+    for (const t = 0; c < columns; c++) {
+      newTiles += "<div class='tile'></div>";
+    }
+    gameGrid.insertAdjacentHTML(
+      "beforeHand",
+      "<div class='row'>" + newTiles + "</div"
+    );
+  }
+
+  //create the bomb count
+  bombsCount = bombs;
+  bombsCounter = innerHTML = bombCount;
+
+  //reset timeer
+  resetTimer();
+}
+
+//begin the game! set up board using code above
+createBoard(gameLevel);
+
+//reset the board using the reset button
+document.querySelector("html").addEventListener("mousedown", function (event) {
+  if (event.target.classList.contains("reset")) {
+    event.target.textContent = "reset";
+  }
+});
+
+document.querySelector("html").addEventListener("mouseup", function (event) {
+  if (event.target.classList.contains("reset")) {
+    event.target.textContent = "reset";
+    stopTimer();
+    gameLevel = difficultyLevel.ariaValueMax;
+    createBoard(gameLevel);
+  }
+});
+
+document.querySelector("html").addEventListener("click", function (event) {
+  if (event.target.classList.contains("status-indicator")) {
+    gameLevel = difficultyLevel.ariaValueMax;
+    createBoard(gameLevel);
+  }
+});
+
+//set the game based on level diffuclty selection
+difficultyLevel.addEventListener("change", function () {
+  stopTimer();
+  resetTimer();
+  gameLevel = difficultyLevel.ariaValueMax;
+  createBoard();
+});
